4eddcf8fedde79e380bc58602da551b9215ac17c
GuilhermeNATA1 <boim.guilherme@escola.pr.gov.br>
2026-04-09 11:30:59 -0300

    ok

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,7 @@
 
-// Importa a função de criação do client Supabase via CDN
+
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'SUPABASE_URL',
   'SUPABASE_ANON_KEY'
